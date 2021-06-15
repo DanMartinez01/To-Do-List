6de31d566754a25b7c4b685bb43b9ec334caec72
Daniela Martinez <martinezdl_@hotmail.com>
2021-06-15 16:27:41 -0300

    First commit

---
+++ assets/JS/script.js
@@ -0,0 +1,32 @@
+
+// DESAPARECE LOS LI AL TACHARLOS
+
+$("span").click(function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+})
+
+// AGREGAR ELEMENTOS A LISTA
+
+$("input[type='text']").keypress(function (event) {
+    //filtrar para que sólo se active cuando aprietan Enter
+    if (event.key == "Enter") {
+        $("ul").append("<li><span><i class='fa fa-trash' aria-hidden='true'></i></span> " + $(this).val() + "</li>");
+        // se borra el contenido del input
+        $(this).val("");
+        event.stopPropagation();
+    }
+});
+
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completado")
+});
+// Borrar los items completados
+$("ul").on("click", "span", function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
